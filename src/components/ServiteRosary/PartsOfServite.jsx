ea125f1e1807a3239948e9e63bedbf317d21dfd3
Barrett Anderson <barrett1614@gmail.com>
2024-11-18 13:10:55 -0600

    Started ServiteDecade

--- src/components/ServiteRosary/PartsOfServite.jsx
+++ src/components/ServiteRosary/PartsOfServite.jsx
@@ -15,7 +15,24 @@ export function BeginningOfServite() {
 }
 
 export function ServiteDecade() {
-  return <div></div>;
+  return (
+    <div>
+      <h2 className="custom-margin-top">THE SERVITE DECADE IS AS FOLLOWS:</h2>
+      <h2>-- DECLARE THE NEXT SORROW</h2>
+      <h2>-- PRAY 1 OUR FATHER</h2>
+      <h2>-- PRAY 7 HAIL MARY'S</h2>
+      <h2>-- PRAY 1 MOST MERCIFUL MOTHER</h2>
+      <h2>-- PRAY 1 DECADE FOR EVERY SORROW</h2>
+      <h2 className="custom-margin-top">THE SEVEN SORROWS OF MARY:</h2>
+      <p>The First Sword of Sorrow: The Prophecy of Simeon</p>
+      <p>The Second Sword of Sorrow: The Flight Into Egypt</p>
+      <p>The Third Sword of Sorrow: The Loss of Jesus in the Temple</p>
+      <p>The Fourth Sword of Sorrow: Mary Meets Jesus on His Way to Calvary</p>
+      <p>The Fifth Sword of Sorrow: Mary Stands at the Foot of the Cross</p>
+      <p>The Sixth Sword of Sorrow: Jesus is Taken Down From the Cross</p>
+      <p>The Seventh Sword of Sorrow: The Burial of Jesus</p>
+    </div>
+  );
 }
 
 export function ClosingOfServite() {
--- src/components/ServiteRosary/ServiteRosaryOffering.jsx
+++ src/components/ServiteRosary/ServiteRosaryOffering.jsx
@@ -19,42 +19,20 @@ export default function ServiteRosaryOffering() {
       <div className="App">
         <h2 className="introText">{otherTexts.introText}</h2>
         <div className={`content ${showContent ? "visible" : ""}`}>
-          {/*  */}
           <BeginningOfServite />
+          <ServiteDecade />
           {/*  */}
-          <h2 className="custom-margin-top">
-            THE SERVITE DECADE IS AS FOLLOWS:
-          </h2>
-          <h2>-- DECLARE THE NEXT SORROW</h2>
-          <h2>-- PRAY 1 OUR FATHER</h2>
-          <h2>-- PRAY 7 HAIL MARY'S</h2>
-          <h2>-- PRAY 1 MOST MERCIFUL MOTHER</h2>
-          <h2>-- PRAY 1 DECADES FOR EVERY SORROW</h2>
-          <>
-            <h2 className="custom-margin-top">THE SEVEN SORROWS OF MARY:</h2>
-            <p>The First Sword of Sorrow: The Prophecy of Simeon</p>
-            <p>The Second Sword of Sorrow: The Flight Into Egypt</p>
-            <p>The Third Sword of Sorrow: The Loss of Jesus in the Temple</p>
-            <p>
-              The Fourth Sword of Sorrow: Mary Meets Jesus on His Way to Calvary
-            </p>
-            <p>
-              The Fifth Sword of Sorrow: Mary Stands at the Foot of the Cross
-            </p>
-            <p>The Sixth Sword of Sorrow: Jesus is Taken Down From the Cross</p>
-            <p>The Seventh Sword of Sorrow: The Burial of Jesus</p>
-            <h2 className="custom-margin-top">THE CLOSING PRAYERS:</h2>
-            <p>
-              PRAY ONCE: Queen of Martyrs, your heart suffered so much. I beg
-              you, by the merits of the tears you shed in these terrible and
-              sorrowful times, to obtain for me and all the sinners of the world
-              the grace of complete sincerity and repentance. Amen.
-            </p>
-            <p>
-              PRAY THRICE: Mary, who was conceived without sin and who suffered
-              for us, pray for us.
-            </p>
-          </>
+          <h2 className="custom-margin-top">THE CLOSING PRAYERS:</h2>
+          <p>
+            PRAY ONCE: Queen of Martyrs, your heart suffered so much. I beg you,
+            by the merits of the tears you shed in these terrible and sorrowful
+            times, to obtain for me and all the sinners of the world the grace
+            of complete sincerity and repentance. Amen.
+          </p>
+          <p>
+            PRAY THRICE: Mary, who was conceived without sin and who suffered
+            for us, pray for us.
+          </p>
           <p className="custom-margin-bottom">{otherTexts.beginningSign}</p>
           <Footer />
         </div>
